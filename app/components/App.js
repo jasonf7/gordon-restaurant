e1143a44acae8f29236edb7277c06f4e7b9d6e8c
jasonf7 <jason.z.fang@gmail.com>
2016-12-13 18:02:14 -0500

    add material ui

--- app/components/App.js
+++ app/components/App.js
@@ -1,15 +1,23 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const App = ({ children }) =>
-    <div>
-        <h1>Filter table</h1>
-        { children }
-        <footer>
-            <Link to="/">Filterable Table</Link>
-            <Link to="/about">About</Link>
-        </footer>
-    </div>;
+	<MuiThemeProvider>
+		<div>
+	        <h1>Filter table</h1>
+	        { children }
+	        <footer>
+	            <Link to="/">Filterable Table</Link>
+	            <Link to="/about">About</Link>
+	        </footer>
+	    </div>
+	</MuiThemeProvider>;
 
 App.propTypes = {
     children: PropTypes.object
